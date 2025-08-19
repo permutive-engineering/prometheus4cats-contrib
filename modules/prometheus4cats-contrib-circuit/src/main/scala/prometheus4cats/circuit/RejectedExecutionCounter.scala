7f600dff2a369a6aaca6c93fb8ad7e200c5fab62
Ondra Pelech <ondra.pelech@gmail.com>
2025-08-19 18:59:55 +0200

    fmt

--- modules/prometheus4cats-contrib-circuit/src/main/scala/prometheus4cats/circuit/RejectedExecutionCounter.scala
+++ modules/prometheus4cats-contrib-circuit/src/main/scala/prometheus4cats/circuit/RejectedExecutionCounter.scala
@@ -4,23 +4,26 @@ import cats.effect._
 import io.chrisdavenport.circuit._
 import prometheus4cats._
 
-trait RejectedExecutionCounter[F[_]]{
+trait RejectedExecutionCounter[F[_]] {
+
   def meteredCircuit(c: CircuitBreaker[F], circuitName: String): CircuitBreaker[F]
-  def meteredResourceCircuit(c: CircuitBreaker[({type lambda[x] = Resource[F, x]})#lambda], circuitName: String): CircuitBreaker[({type lambda[x] = Resource[F, x]})#lambda]
+
+  def meteredResourceCircuit(
+      c: CircuitBreaker[({ type lambda[x] = Resource[F, x] })#lambda],
+      circuitName: String
+  ): CircuitBreaker[({ type lambda[x] = Resource[F, x] })#lambda]
+
 }
 
 object RejectedExecutionCounter {
 
-  /**
-   * Initialization of the Generalized Modifier
-   * which can be applied to multiple circuit breakers.
-   */
+  /** Initialization of the Generalized Modifier which can be applied to multiple circuit breakers.
+    */
   def register[F[_]](
-    mr: MetricRegistry[F],
-    metricName: Counter.Name = "circuit_rejected_execution_total"
+      mr: MetricRegistry[F],
+      metricName: Counter.Name = "circuit_rejected_execution_total"
   ): Resource[F, RejectedExecutionCounter[F]] =
-    MetricFactory
-      .builder
+    MetricFactory.builder
       .build(mr)
       .counter(metricName)
       .ofLong
@@ -30,24 +33,27 @@ object RejectedExecutionCounter {
       .map(new DefaultRejectedExecutionCounter(_))
 
   private class DefaultRejectedExecutionCounter[F[_]](
-   counter: Counter[F, Long, String]
-  ) extends RejectedExecutionCounter[F]{
+      counter: Counter[F, Long, String]
+  ) extends RejectedExecutionCounter[F] {
+
     override def meteredCircuit(c: CircuitBreaker[F], circuitName: String): CircuitBreaker[F] =
       c.doOnRejected(counter.inc(circuitName))
-    override def meteredResourceCircuit(c: CircuitBreaker[({type lambda[x] = Resource[F, x]})#lambda], circuitName: String): CircuitBreaker[({type lambda[x] = Resource[F, x]})#lambda] =
+
+    override def meteredResourceCircuit(
+        c: CircuitBreaker[({ type lambda[x] = Resource[F, x] })#lambda],
+        circuitName: String
+    ): CircuitBreaker[({ type lambda[x] = Resource[F, x] })#lambda] =
       c.doOnRejected(counter.mapK(Resource.liftK).inc(circuitName))
+
   }
 
-  /**
-   * Single Metered Circuit
-   */
+  /** Single Metered Circuit */
   def meteredCircuit[F[_]](
-    mr: MetricRegistry[F],
-    metricName: Counter.Name,
-    circuit: CircuitBreaker[F]
+      mr: MetricRegistry[F],
+      metricName: Counter.Name,
+      circuit: CircuitBreaker[F]
   ): Resource[F, CircuitBreaker[F]] =
-    MetricFactory
-      .builder
+    MetricFactory.builder
       .build(mr)
       .counter(metricName)
       .ofLong
